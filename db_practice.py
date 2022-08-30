8c3e30d3f3b1b393d4aa964a2f8e3427a0b8ceb5
JLopez2042 <jacob.lopez.2042@gmail.com>
2022-08-30 16:07:02 -0600

    Create db_practice.py
    Created a script that created a database when determining which files ended in .txt and adding only those to the database.

---
+++ db_practice.py
@@ -0,0 +1,33 @@
+
+import sqlite3
+
+conn = sqlite3.connect('test2.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fileName TEXT \
+        )")
+    conn.commit()
+conn.close()
+
+
+conn = sqlite3.connect('test2.db')
+
+# tuple of files
+file_tuple = ('information.docx', 'Hello.txt', 'myImage.png', \
+              'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+#loop through each object in the tuple to find files that end in .txt
+for x in file_tuple:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+        # The value for each row will be one name out of the tuple therefore (x,)
+        # will denote a one element tuple for each file ending with .txt
+            cur.execute("INSERT INTO tbl_files(col_fileName) VALUES (?)", (x,))
+            print(x)
+conn.close()
+
+
