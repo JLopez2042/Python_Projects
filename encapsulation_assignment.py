a7ba60c6bf7a76201bfcf04120eca5a28d97f401
JLopez2042 <jacob.lopez.2042@gmail.com>
2022-09-27 09:04:10 -0700

    created encapsulation_assignment.py
    practicing encapsulation

---
+++ encapsulation_assignment.py
@@ -0,0 +1,25 @@
+
+# Creating a protected variable
+class Protected:
+    def __init__(self):
+        self._protectedVar = 0
+
+obj = Protected()
+obj._protectedVar = 34
+print(obj._protectedVar)
+
+# Creating a private variable
+class Protected:
+    def __init__(self):
+        self.__privateVar = 12
+    
+    def getPrivate(self):
+        print(self.__privateVar)
+    
+    def setPrivate(self, private):
+        self.__privateVar = private
+
+obj = Protected()
+obj.getPrivate()
+obj.setPrivate(23)
+obj.getPrivate()
