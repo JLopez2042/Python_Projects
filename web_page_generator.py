b699bceb2223f2bf7d060412bd9ac43c520818f3
JLopez2042 <jacob.lopez.2042@gmail.com>
2022-09-28 12:37:11 -0700

    update web_page_generator.py

--- web_page_generator.py
+++ web_page_generator.py
@@ -19,8 +19,8 @@ class ParentWindow(Frame):
         
         label = tk.Label(text="Custom Text Entry")
         label.grid(row=2, column=1, padx=(10,10), pady=(10,10))
-        entry = tk.Entry()
-        entry.grid(row=2, column=2, padx=(10,10), pady=(10,10))
+        self.entry = tk.Entry()
+        self.entry.grid(row=2, column=2, padx=(10,10), pady=(10,10))
         
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
@@ -31,10 +31,10 @@ class ParentWindow(Frame):
         webbrowser.open_new_tab("index.html")
 
     def customText(self):
-        htmlText2 = Entry.get()
+        htmlText2 = self.entry.get()
         htmlFile = open("index.html", "w")
-        htmlContent = "<html>\n<body>" + htmlText2 + "</body>\n</html>"
-        htmlFile.write()
+        htmlContent2 = "<html>\n<body>" + htmlText2 + "</body>\n</html>"
+        htmlFile.write(htmlContent2)
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
     
